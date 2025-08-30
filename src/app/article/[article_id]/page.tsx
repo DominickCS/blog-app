8c104d300c22ccb65e30906c63aff9b8872409d4
Dominick <dominick.smith938@gmail.com>
2025-08-30 08:44:38 -0500

    fixed unauth article reading function

--- src/app/article/[article_id]/page.tsx
+++ src/app/article/[article_id]/page.tsx
@@ -1,34 +1,30 @@
 'use client'
+import { fetchArticles } from "@/app/actions/articles";
 import { verifySession } from "@/app/lib/dal";
 import { GetUserArticles } from "@/app/profile/actions";
 import { usePathname } from "next/navigation";
 import { useEffect, useState } from "react"
 export default function Article() {
-  const [userArticles, setUserArticles] = useState(null)
+  const [article, setArticle] = useState(null)
   const uri = usePathname()
   let splituri = uri.split('/')
-  console.log(splituri)
 
   useEffect(() => {
-    async function fetchSession() {
-      const currentSession = await verifySession()
-      const articles = await GetUserArticles(currentSession.userId)
-      setUserArticles(articles)
-
+    async function getArticle() {
+      const currentArticle = await fetchArticles()
+      setArticle(currentArticle)
     }
-    fetchSession()
+    getArticle()
+  }, [])
 
-  }, []);
-
-  console.log(userArticles)
   return (
     <>
       <section>
-        {userArticles ?
+        {article ?
           <div className="flex flex-col items-center m-24 border-2 border-black rounded p-72">
-            <h1>{userArticles[splituri[2] - 1]?.article_title}</h1>
-            <p>{userArticles[splituri[2] - 1]?.article_body}</p>
-            <p>Likes: {userArticles[splituri[2] - 1]?.like_count}</p>
+            <h1>{article[splituri[2] - 1]?.article_title}</h1>
+            <p>{article[splituri[2] - 1]?.article_body}</p>
+            <p>Likes: {article[splituri[2] - 1]?.like_count}</p>
           </div>
           :
           <h1>Loading Article</h1>}
